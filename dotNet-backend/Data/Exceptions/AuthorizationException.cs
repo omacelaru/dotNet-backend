ac8c4905799b9276f500b4f7aa40b575e40531c9
Victor Cucu <victorstefan28@gmail.com>
2023-12-25 22:57:11 +0200

    Added User model, created Authentication Service based on Identity framework.

---
+++ dotNet-backend/Data/Exceptions/AuthorizationException.cs
@@ -0,0 +1,7 @@
+﻿namespace dotNet_backend.Data.Exceptions
+{
+    public class AuthorizationException : Exception
+    {
+        public AuthorizationException(string message) : base(message) { }
+    }
+}
---
+++ dotNet-backend/Models/User/User.cs
@@ -0,0 +1,24 @@
+﻿using dotNet_backend.Models.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotNet_backend.Models.User
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public bool EmailConfirmed { get; set; } = false;
+
+    }
+
+}
