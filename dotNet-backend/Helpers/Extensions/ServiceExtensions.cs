9eb6307ac397b69681031d35962c7d771a5e17e3
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-27 15:28:08 +0200

    refactor Program.cs

--- dotNet-backend/Helpers/Extensions/ServiceExtensions.cs
+++ dotNet-backend/Helpers/Extensions/ServiceExtensions.cs
@@ -1,6 +1,10 @@
-﻿using dotNet_backend.Repositories.UserRepository;
+﻿using System.Text;
+using dotNet_backend.Repositories.UserRepository;
 using dotNet_backend.Services.AuthService;
 using dotNet_backend.Services.SMTP;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace dotNet_backend.Helpers.Extensions
 {
@@ -17,6 +21,58 @@ namespace dotNet_backend.Helpers.Extensions
         {
             services.AddTransient<IAuthService, AuthService>();
             services.AddTransient<ISMTPService, SMTPService>();
+            return services;
+        }
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Karate API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddAuthentications(this IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+
+                    var tokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("JWT:Key")),
+
+                        ClockSkew = TimeSpan.Zero
+                    };
+                    options.TokenValidationParameters = tokenValidationParameters;
+                });
             return services;
         }
     }
