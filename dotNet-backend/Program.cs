e23a2cf04065e7f173e7c9ad1119491c6185b1f2
Gherca Darius <ghercadarius@gmail.com>
2023-12-27 09:54:12 +0200

    added smtp service

--- dotNet-backend/Program.cs
+++ dotNet-backend/Program.cs
@@ -1,11 +1,93 @@
+using dotNet_backend.Auth;
+using dotNet_backend.Data.Filters;
+using dotNet_backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using dotNet_backend.Helpers;
+using dotNet_backend.Helpers.Extensions;
+using Serilog;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using dotNet_backend.Services.SMTP;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+void ConfigureServices(WebApplicationBuilder builderInstance)
+{
+    var configuration = builderInstance.Configuration;
+    Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+        .WriteTo.File("log/KarateLogs.txt", rollingInterval: RollingInterval.Month)
+        .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builderInstance.Services.AddControllers(options =>
+    {
+        ///options.Filters.Add(new EmailVerifiedFilter());
+    });
+    builderInstance.Host.UseSerilog();
+    builderInstance.Services.AddAutoMapper(typeof(MapperProfile));
+    builderInstance.Services.AddControllers();
+    builderInstance.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+    builderInstance.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+    builderInstance.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidAudience = configuration["JWT:ValidAudience"],
+                ValidIssuer = configuration["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]))
+            };
+        });
+    builderInstance.Services.AddTransient<ISMTPService, SMTPService>();
+    builderInstance.Services.AddRepositories();
+    builderInstance.Services.AddServices();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builderInstance.Services.AddEndpointsApiExplorer();
+
+    builderInstance.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Karate API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
 
 var app = builder.Build();
 
@@ -20,6 +102,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+    //add smtp with sengrid
+    app.UseSendGridEmailSender();
+
+    app.MapControllers();
 
 app.Run();
---
+++ dotNet-backend/Services/SMTP/SMTPService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace dotNet_backend.Services.SMTP
+{
+    public class SMTPService : ISMTPService
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger logger;
+
+        public SMTPService(IConfiguration configuration, ILogger<SMTPService> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string sendGridApiKey = configuration["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"];
+            if (string.IsNullOrEmpty(sendGridApiKey))
+            {
+                throw new Exception("The 'SendGridApiKey' is not configured");
+            }
+
+            var client = new SendGridClient(sendGridApiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("[ghercadarius@gmail.com]", "[karate_test]"),
+                Subject = subject,
+                PlainTextContent = htmlMessage,
+                HtmlContent = htmlMessage
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            var response = await client.SendEmailAsync(msg);
+            if (response.IsSuccessStatusCode)
+            {
+                logger.LogInformation("Email queued successfully");
+            }
+            else
+            {
+                logger.LogError("Failed to send email");
+                // Adding more information related to the failed email could be helpful in debugging failure,
+                // but be careful about logging PII, as it increases the chance of leaking PII
+            }
+
+        }
+    }
+}
