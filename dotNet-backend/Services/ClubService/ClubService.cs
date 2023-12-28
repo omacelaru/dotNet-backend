e1c20b29f1c47b81dbf442eec1bceba42aaad1bb
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-28 20:18:23 +0200

    add club to coaches

--- dotNet-backend/Services/ClubService/ClubService.cs
+++ dotNet-backend/Services/ClubService/ClubService.cs
@@ -1,33 +1,41 @@
 ﻿using AutoMapper;
 using dotNet_backend.Controllers;
 using dotNet_backend.Models.Club;
+using dotNet_backend.Models.Club.DTO;
+using dotNet_backend.Models.Coach;
 using dotNet_backend.Repositories.ClubRepository;
+using dotNet_backend.Repositories.CoachRepository;
 
 namespace dotNet_backend.Services.ClubService
 {
     public class ClubService : IClubService
     {
         private readonly IClubRepository _clubRepository;
+        private readonly ICoachRepository _coachRepository;
         private readonly IMapper _mapper;
 
-        public ClubService(IClubRepository clubRepository, IMapper mapper)
+        public ClubService(IClubRepository clubRepository, ICoachRepository coachRepository, IMapper mapper)
         {
             _clubRepository = clubRepository;
+            _coachRepository = coachRepository;
             _mapper = mapper;
         }
 
-        public async Task<Club> CreateClubAsync(Club club)
+        public async Task<ClubResponseDto> CreateClubAsync(string coachUsername,Club club)
         {
+            var coach = await _coachRepository.FindByUserNameAsync(coachUsername);
+            club.Coaches = new List<Coach> {coach};
             await _clubRepository.CreateAsync(club);
             await _clubRepository.SaveAsync();
-            return club;
+            return _mapper.Map<ClubResponseDto>(club);
         }
 
-        public async Task<IEnumerable<Club>> GetAllClubsAsync()
+        public async Task<IEnumerable<ClubResponseDto>> GetAllClubsAsync()
         {
-            return await _clubRepository.GetAllAsync();
+            //TODO - fix coach names
+            var clubs = await _clubRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ClubResponseDto>>(clubs);
         }
-
-
+        
     }
 }
