a7fb51490e4f080607f855537f3e93f7f50d9dd8
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-28 20:19:28 +0200

    add CoachService

---
+++ dotNet-backend/Services/CoachService/CoachService.cs
@@ -0,0 +1,20 @@
+﻿using dotNet_backend.Models.Club;
+using dotNet_backend.Models.Coach;
+using dotNet_backend.Models.Coach.DTO;
+using dotNet_backend.Repositories.ClubRepository;
+using dotNet_backend.Repositories.CoachRepository;
+
+namespace dotNet_backend.Services.CoachService;
+
+public class CoachService : ICoachService
+{
+    private readonly ICoachRepository _coachRepository;
+    private readonly IClubRepository _clubRepository;
+
+    public CoachService(ICoachRepository coachRepository, IClubRepository clubRepository)
+    {
+        _coachRepository = coachRepository;
+        _clubRepository = clubRepository;
+    }
+    
+}
