2328a34fb74bfb74233dd3784cddbb2d203f1eec
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-28 20:18:57 +0200

    add CoachService

--- dotNet-backend/Services/ClubService/IClubService.cs
+++ dotNet-backend/Services/ClubService/IClubService.cs
@@ -1,10 +1,11 @@
 ﻿using dotNet_backend.Models.Club;
+using dotNet_backend.Models.Club.DTO;
 
 namespace dotNet_backend.Services.ClubService
 {
     public interface IClubService
     {
-        Task<Club> CreateClubAsync(Club club);
-        Task<IEnumerable<Club>> GetAllClubsAsync();
+        Task<ClubResponseDto> CreateClubAsync(string coachUserName, Club club);
+        Task<IEnumerable<ClubResponseDto>> GetAllClubsAsync();
     }
 }
---
+++ dotNet-backend/Services/CoachService/ICoachService.cs
@@ -0,0 +1,9 @@
+﻿using dotNet_backend.Models.Club;
+using dotNet_backend.Models.Coach;
+using dotNet_backend.Models.Coach.DTO;
+
+namespace dotNet_backend.Services.CoachService;
+
+public interface ICoachService
+{
+}
