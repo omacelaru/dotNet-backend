41908e868f5b75082aa2ba47c1c22361cdf7d023
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-28 20:18:43 +0200

    add CoachRepository

---
+++ dotNet-backend/Repositories/CoachRepository/CoachRepository.cs
@@ -0,0 +1,18 @@
+﻿using dotNet_backend.Models.Coach;
+using dotNet_backend.Repositories.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNet_backend.Repositories.CoachRepository;
+
+public class CoachRepository : GenericRepository<Coach>, ICoachRepository
+{
+    public CoachRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    // Add custom methods here
+    public async Task<Coach> FindByUserNameAsync(string coachUsername)
+    {
+        return await _table.FirstOrDefaultAsync(c => c.Username == coachUsername);
+    }
+}
---
+++ dotNet-backend/Repositories/CoachRepository/ICoachRepository.cs
@@ -0,0 +1,9 @@
+﻿using dotNet_backend.Models.Coach;
+using dotNet_backend.Repositories.GenericRepository;
+
+namespace dotNet_backend.Repositories.CoachRepository;
+
+public interface ICoachRepository : IGenericRepository<Coach>
+{
+    Task<Coach> FindByUserNameAsync(string coachUsername);
+}
