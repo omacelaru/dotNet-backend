aad7a28ecf023e62d4602232ff43ee97528e54e6
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-28 20:19:48 +0200

    fix UserRepository

--- dotNet-backend/Repositories/UserRepository/UserRepository.cs
+++ dotNet-backend/Repositories/UserRepository/UserRepository.cs
@@ -6,16 +6,10 @@ namespace dotNet_backend.Repositories.UserRepository
 {
     public class UserRepository : GenericRepository<User>, IUserRepository
     {
-        private readonly ApplicationDbContext _context;
-
-        public UserRepository(ApplicationDbContext context) : base(context)
-        {
-            _context = context;
-        }
-        
+        public UserRepository(ApplicationDbContext context) : base(context){}
         public async Task<User> GetByEmailAsync(string email)
         {
-            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            return await _table.FirstOrDefaultAsync(u => u.Email == email);
         }
     }
 }
