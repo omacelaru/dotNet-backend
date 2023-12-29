02fd372d330635ac3b9d42d236d9bc0bffe156c5
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-29 18:34:01 +0200

    add athletes names

--- dotNet-backend/Models/Coach/DTO/CoachResponseDto.cs
+++ dotNet-backend/Models/Coach/DTO/CoachResponseDto.cs
@@ -6,5 +6,7 @@ namespace dotNet_backend.Models.Coach.DTO
     {
         public string Name { get; set; }
         public string ClubName { get; set; }
+
+        public ICollection<string> AthletesNames { get; set; }
     }
 }
