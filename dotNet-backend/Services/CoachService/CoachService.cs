554a30da1d2ad69bb21453c56cfb67f59ede796e
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-12-29 13:53:19 +0200

    add coach controller

--- dotNet-backend/Services/CoachService/CoachService.cs
+++ dotNet-backend/Services/CoachService/CoachService.cs
@@ -1,4 +1,5 @@
-﻿using dotNet_backend.Models.Club;
+﻿using dotNet_backend.Data.Exceptions;
+using dotNet_backend.Models.Club;
 using dotNet_backend.Models.Coach;
 using dotNet_backend.Models.Coach.DTO;
 using dotNet_backend.Repositories.ClubRepository;
@@ -16,5 +17,29 @@ public class CoachService : ICoachService
         _coachRepository = coachRepository;
         _clubRepository = clubRepository;
     }
+
+    public async Task<Club> AddCoachToClubAsync(string coachUserName,Club club)
+    {
+        var coach = await _coachRepository.FindByUserNameAsync(coachUserName);
+        if(coach.ClubId.HasValue)
+            throw new CoachAlreadyHasAClub();
+        var clubToAdd = await _clubRepository.FindByIdAsync(club.Id);
+        if(clubToAdd == null)
+            throw new Exception();
+        clubToAdd.Coaches = new List<Coach>() { coach };
+        coach.ClubId = clubToAdd.Id;
+        _coachRepository.Update(coach);
+        await _coachRepository.SaveAsync();
+        return clubToAdd;
+    }
     
+    public async Task<IEnumerable<Coach>> GetAllCoachesAsync()
+    {
+        return await _coachRepository.GetAllAsync();
+    }
+
+    public async Task<Coach> GetCoachByIdAsync(Guid id)
+    {
+        return await _coachRepository.FindByIdAsync(id);
+    }
 }
--- dotNet-backend/Services/CoachService/ICoachService.cs
+++ dotNet-backend/Services/CoachService/ICoachService.cs
@@ -6,4 +6,7 @@ namespace dotNet_backend.Services.CoachService;
 
 public interface ICoachService
 {
+    Task<Club> AddCoachToClubAsync(string coachUserName, Club club);
+    Task<IEnumerable<Coach>> GetAllCoachesAsync();
+    Task<Coach> GetCoachByIdAsync(Guid id);
 }
