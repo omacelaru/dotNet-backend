0358206170233aa6ddd20487882e0515b2cb8704
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-13 12:47:03 +0200

    fix RequestDtos

---
+++ dotNet-backend/Models/Request/DTO/RequestInfoResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace dotNet_backend.Models.Request.DTO
+{
+    public class RequestInfoResponseDto
+    {
+        //CreateMap<RequestInfo, RequestInfoResponseDto>()
+        // .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.RequestByUser));
+        public string Name { get; set; }
+    }
+
+}
