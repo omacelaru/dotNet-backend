3318af64f2590a7d13b097a1290f7bffc42153ff
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-26 11:17:28 +0200

    fix coach controller

--- dotNet-backend/Services/CoachService/ICoachService.cs
+++ dotNet-backend/Services/CoachService/ICoachService.cs
@@ -2,14 +2,14 @@
 using dotNet_backend.Models.Coach;
 using dotNet_backend.Models.Coach.DTO;
 using dotNet_backend.Models.Request;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotNet_backend.Services.CoachService;
 
 public interface ICoachService
 {
-    Task<IEnumerable<Coach>> GetAllCoachesAsync();
-    Task<Coach> GetCoachByIdAsync(Guid id);
-    Task<Coach> GetCoachByUserNameAsync(string username);
+    Task<ActionResult<IEnumerable<CoachResponseDto>>> GetAllCoachesAsync();
+    Task<ActionResult<CoachResponseDto>> GetCoachByIdAsync(Guid id);
+    Task<ActionResult<CoachResponseDto>> GetCoachByUserNameAsync(string username);
     Task AddAthleteToCoach(string usernameAthlete, string usernameCoach);
-    Task UpdateCoachAsync(Coach newCoach);
 }
