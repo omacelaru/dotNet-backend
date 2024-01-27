e47c72023cb72cd6f31b408a92fb1e6ff48d90fc
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-27 14:40:24 +0200

    delete unnecessary exceptions

--- dotNet-backend/Exceptions/UsernameAlreadyExists.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dotNet_backend.Data.Exceptions;
-
-public class UsernameAlreadyExists : Exception
-{
-    public UsernameAlreadyExists() : base("Username already exists") {}
-    
-    public UsernameAlreadyExists(string message) : base(message) {}
-    
-}
