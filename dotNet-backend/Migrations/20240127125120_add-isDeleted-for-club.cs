b00c2ed41db3093580083de2b900a66d6ffd0676
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-27 14:52:52 +0200

    add a new column (isDeleted) in club model

---
+++ dotNet-backend/Migrations/20240127125120_add-isDeleted-for-club.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotNet_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class addisDeletedforclub : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Clubs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Clubs");
+        }
+    }
+}
--- dotNet-backend/Models/Club/Club.cs
+++ dotNet-backend/Models/Club/Club.cs
@@ -8,5 +8,7 @@ namespace dotNet_backend.Models.Club
 
         public Guid CoachId { get; set; }
         public Coach.Coach Coach { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
     }
 }
