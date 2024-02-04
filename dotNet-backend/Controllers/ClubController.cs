818eaeaf6d8d790406b538585a555b681a468cfa
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-02-04 15:50:19 +0200

    fix ClubController.cs

--- dotNet-backend/Controllers/ClubController.cs
+++ dotNet-backend/Controllers/ClubController.cs
@@ -12,21 +12,41 @@ namespace dotNet_backend.Controllers
     {
         private readonly IClubService _clubService = clubService;
         
+        /// <summary>
+        /// Get all clubs
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Clubs found</response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ClubResponseDto>>> GetAllClubs() =>
             await _clubService.GetAllClubsAsync();
 
+        /// <summary>
+        /// Create a club from a coach account
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Club created</response>
         [HttpPost]
         [Authorize(Roles = "Coach")]
         [ValidateModel]
         public async Task<ActionResult<ClubResponseDto>> CreateClubFromCoach([FromBody] ClubRequestDto clubRequestDto) => 
             await _clubService.CreateClubAsync(clubRequestDto, User.Identity.Name);
         
+        /// <summary>
+        /// Delete a club by a coach
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Club deleted</response>
         [HttpDelete]
         [Authorize(Roles = "Coach")]
         public async Task<ActionResult<ClubResponseDto>> DeleteClubFromCoach() => 
             await _clubService.DeleteClubAsync(User.Identity.Name);
         
+        /// <summary>
+        /// Update a club name by a coach
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Club updated</response>
         [HttpPatch]
         [Authorize(Roles = "Coach")]
         [ValidateModel]
